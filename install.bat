2b431cc5aaae913ed1aa77f614729dba54a72a7d
Jan Miran Karaozan <87015514+JanMiranKaraozan@users.noreply.github.com>
2024-07-10 16:24:44 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
-@echo off
+#!/bin/bash
 
-REM Python yorumlayıcısının yolu
-set PYTHON=python
+# Python yorumlayıcısının yolunu tanımla (eğer python3 gibi bir yorumlayıcı kullanıyorsanız)
+PYTHON=python
 
-REM Gereken kütüphaneleri yüklemek için pip komutları
-%PYTHON% -m pip install opencv-python
-%PYTHON% -m pip install numpy
+# Pip'i güncelle
+$PYTHON -m pip install --upgrade pip
 
-REM Kütüphanelerin başarıyla yüklendiğini belirten mesaj
-echo OpenCV ve NumPy kutuphaneleri basariyla yuklendi. Herhangi bir tusa basarak cikin.
-pause
+# Gereken kütüphaneleri yükle
+$PYTHON -m pip install opencv-python
+$PYTHON -m pip install numpy
+
+# Kütüphanelerin başarıyla yüklendiğini belirten mesajı yazdır
+echo "Pip güncellendi, OpenCV ve NumPy kütüphaneleri başarıyla yüklendi."
